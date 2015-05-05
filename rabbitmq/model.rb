1a38f41b833e8fdb9b88f7dae26accd47e428ba4
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2015-05-05 12:52:19 +0900

    view added and view code to view.rb refactoring

--- rabbitmq/model.rb
+++ rabbitmq/model.rb
@@ -134,27 +134,3 @@ class Host < Model
     hosts
   end
 end
-
-if __FILE__ == $0
-  puts "reading channels"
-  channels = Channels.new.read
-  puts "reading consumers"
-  consumers = Consumers.new.read
-  puts "reading bindings"
-  bindings = Bindings.new.read
-
-  puts "normalize infomation(inject_process)"
-  channels.inject_process(Host.read_hosts)
-  puts "normalize infomation(inject_channels)"
-  consumers.inject_channels(channels)
-
-  puts "analyzing"
-  bindings.exchanges.each do |ex|
-    puts "EXCHANGE = #{ex} and its queues"
-    queues = bindings.find_queues_by_exchange_name(ex)
-    queues.each do |q|
-      con = consumers.connection_of_queue(q)
-      puts "  QUEUE = #{q}, con=#{con["ip"]}:#{con["port"]}, cmd=#{con["cmd"]}"
-    end
-  end
-end

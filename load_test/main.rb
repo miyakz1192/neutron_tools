db6935f6b29b0c480bff0a82c4ba41f769eaa746
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2015-12-29 10:55:22 +0900

    some change

--- load_test/main.rb
+++ load_test/main.rb
@@ -1,9 +1,10 @@
 
 net = "bf285ec8-0e33-4482-b1a9-82a7526c11c2"
 count_max=1
-parallel_max=50
+parallel_max=70
 
 for count in 1..count_max do 
+  puts "TEST No #{count} start"
   threads = []
   for parallel in 1..parallel_max do
     t = Thread.start(parallel) do |_parallel|
@@ -16,9 +17,10 @@ for count in 1..count_max do
     end
     threads << t
   end 
-  threads.each do |t|
-    t.join
+  threads.each do |tm|
+    tm.join
   end
+  puts "TEST No #{count} end"
 end
 
 

d7f33edb6eb003b91cc163c4f9aad4a3cd5efb8a
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2016-02-14 13:01:52 +0900

    updated

--- integration_test/sample.rb
+++ integration_test/sample.rb
@@ -3,98 +3,204 @@ require 'pp'
 require 'logger'
 
 class TestEnvironmentBase
+  @@auth_url = ""
+
+  # input authentication info
+  # @param params[:user_name] [String] test user name
+  # @param params[:password] [String] test user password
+  # @param params[:tenant_name] [String] test tenant_name
   def initialize(params = {})
     @logger = Logger.new(STDOUT)
+    @auth_info = {
+      :provider => 'OpenStack',
+      :openstack_api_key => params[:password],
+      :openstack_username => params[:user_name],
+      :openstack_auth_url => "#{@@auth_url}/tokens",
+      :openstack_tenant => params[:tenant_name]
+    }
+  end
+
+  def create(params = {})
+    logger.info("*** [START] create@#{self.class.name} ***")
+    create_impl(params)
+    logger.info("*** [END] create@#{self.class.name} ***")
+  end
+  
+  def delete(params = {})
+    logger.info("*** [START] delete@#{self.class.name} ***")
+    delete_impl(params)
+    logger.info("*** [END] delete@#{self.class.name} ***")
+  end
+
+  def self.auth_url=(url)
+    @@auth_url = url
+  end
+
+protected
+  def logger
+    @logger
+  end
+
+  def auth_info
+    @auth_info
   end
 end
 
 class IdentityTestEnvironment < TestEnvironmentBase
   #initialize
-  #params are equals to admin
+  #params must be admin_auth_info
   def initialize(params)
-    super
-    @keystone = Fog::Identity.new(params)
+    super(params)
+    #params set to auth_info
+    @keystone = Fog::Identity.new(auth_info) 
   end
 
   # ensure given user_name/tenant_name exists
   # @param params[:user_name] [String] test user name
   # @param params[:password] [String] test user password
   # @param params[:tenant_name] [String] test tenant_name
-  def ensure(params)
+  def create_impl(params)
     user_name   = params[:user_name]
     password    = params[:password]
     tenant_name = params[:tenant_name]
 
-    @logger.info("check tenant \"#{tenant_name}\" exists")
+    logger.info("check tenant \"#{tenant_name}\" exists")
     unless keystone.tenants.detect{|t| t.name == tenant_name}
-      @logger.info("#{tenant_name} does not exist. so make it")
+      logger.info("#{tenant_name} does not exist. so make it")
       keystone.tenants.create({:name => tenant_name})
     end
 
-    @logger.info("check user \"#{user_name}\" exists")
+    logger.info("check user \"#{user_name}\" exists")
     unless keystone.users.detect{|u| u.name == user_name}
-      @logger.info("#{user_name} does not exist. so make it")
+      logger.info("#{user_name} does not exist. so make it")
       tenant_id = keystone.tenants.detect{|t| t.name == tenant_name}.id
       keystone.users.create({:name => user_name,
                              :tenant_id => tenant_id,
                              :password => password})
     end
-    @logger.info("show users")
-    keystone.users.each do |user|
-      @logger.info(user.name)
-    end
-    @logger.info("show tenants")
-    keystone.tenants.each do |t|
-      @logger.info(t.name)
-    end
+    logging_res
+  end
+
+  # delete given user_name/tenant_name
+  # @param params[:user_name] [String] test user name
+  # @param params[:password] [String] test user password
+  # @param params[:tenant_name] [String] test tenant_name
+  def delete_impl(params)
+    user_name   = params[:user_name]
+    tenant_name = params[:tenant_name]
+
+    user   = keystone.users.detect{|u| u.name == user_name}
+    tenant = keystone.tenants.detect{|t| t.name == tenant_name}
+
+    user.destroy if user
+    tenant.destroy if tenant
+
+    logging_res
   end
 
 protected
   def keystone
     @keystone
   end
+
+  def logging_res
+    logger.info("===show users===")
+    keystone.users.each do |user|
+      logger.info(user.name)
+    end
+    logger.info("===show tenants===")
+    keystone.tenants.each do |t|
+      logger.info(t.name)
+    end
+  end
 end
 
 class ImageTestEnvironment < TestEnvironmentBase
-  # test user_name/password/tenant_name
+  IMAGE_FILE_DIR = "#{File.expand_path(File.dirname(__FILE__))}/materials/images"
+
+  # params is test_auth_info
   # @param params[:user_name] [String] test user name
   # @param params[:password] [String] test user password
   # @param params[:tenant_name] [String] test tenant_name
   def initialize(params)
-    @glance = Fog::Image.new(params)
+    super(params)
+    @glance = Fog::Image.new(auth_info)
+  end
+
+  # ensure specified image exists. 
+  # user ensure image exists in matarials/images
+  # @param params[:image_name] [String] image_name
+  def create_impl(params = {})
+    image_name = params[:image_name]
+    image_location = "#{IMAGE_FILE_DIR}/#{image_name}"
+    image_size = File.size(image_location)
+
+    logger.info("check image \"#{image_name}\" exists")
+    unless glance.images.detect{|i| i.name == image_name}
+      logger.info("crate image \"#{image_name}\"")
+      glance.images.create({:name => image_name,
+                            #:size => image_size,
+                            :disk_format => "qcow2",
+                            :container_format => "bare",
+                            :location => image_location})
+      logger.info("crate image \"#{image_name}\" done. check exists")
+    end
+
+    logging_res(image_name)
+  end
+
+  def delete_impl(params = {})
+    image_name = params[:image_name]
+
+    image = glance.images.detect{|i| i.name == image_name}
+    image.destroy if image
+    logging_res(image_name)
   end
 
 protected
   def glance
     @glance
   end
+
+  def logging_res(image_name)
+    logger.info("===show images===")
+    image = glance.images.detect{|i| i.name == image_name}
+    if image
+      logger.info("check res == #{image.name}")
+    else
+      logger.info("no such as image #{image_name}")
+    end
+  end
 end
 ##############################################################################
 #  config area
 ##############################################################################
-#define OpenStack admin previlege info
-admin_previledge_info = {
-  :provider => 'OpenStack',
-  :openstack_api_key => ENV['OS_PASSWORD'],
-  :openstack_username => ENV["OS_USERNAME"],
-  :openstack_auth_url => "#{ENV["OS_AUTH_URL"]}/tokens",
-  :openstack_tenant => ENV["OS_TENANT_NAME"]
-}
-#define test previlege info
-test_previledge_info = {:user_name => "test_user", 
-                        :password => "a", 
-                        :tenant_name => "test"}
+#define OpenStack admin authentication info
+admin_auth_info = {:user_name => "admin",
+                   :password => "a", 
+                   :tenant_name => "admin"}
+#define test authentication info
+test_auth_info  = {:user_name => "test_user", 
+                   :password => "a", 
+                   :tenant_name => "test"}
+#auth url
+TestEnvironmentBase.auth_url = "http://192.168.122.84:5000/v2.0"
 #instance image
 test_image = "cirros-0.3.4-x86_64-disk.img"
+# cirros-0.3.4-x86_64-disk.img is available following url
+# wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
 ##############################################################################
 #  code area
 ##############################################################################
 # for OpenStack admin privilege
-idenv = IdentityTestEnvironment.new(admin_previledge_info)
-idenv.ensure(test_previledge_info)
+id_env = IdentityTestEnvironment.new(admin_auth_info)
+id_env.create(test_auth_info)
 
-glance = Fog::Image.new(admin_previledge_info)
-puts glance.images.inspect
+image_env = ImageTestEnvironment.new(test_auth_info)
+image_env.create({:image_name => test_image})
+
+image_env.delete({:image_name => test_image})
+id_env.delete(test_auth_info)
 
 ##for test privilege
 #nova = Fog::Compute.new({
